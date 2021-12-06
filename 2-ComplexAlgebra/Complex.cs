3dd3d38ea6b269a9cda45aaca2cc93c29805e4c9
Anny Bevilacqua - anny.bevilacqua@studio.unibo.it <anny.bevilacqua@studio.unibo.it>
2021-12-06 12:45:07 +0100

    Resolve TODOs in ComplexAlgebra

--- 2-ComplexAlgebra/Complex.cs
+++ 2-ComplexAlgebra/Complex.cs
@@ -1,22 +1,53 @@
+using System;
+using System.ComponentModel.Design;
+using System.Linq;
+
 namespace ComplexAlgebra
 {
     /// <summary>
     /// A type for representing Complex numbers.
     /// </summary>
     ///
-    /// TODO: Model Complex numbers in an object-oriented way and implement this class.
-    /// TODO: In other words, you must provide a means for:
-    /// TODO: * instantiating complex numbers
-    /// TODO: * accessing a complex number's real, and imaginary parts
-    /// TODO: * accessing a complex number's modulus, and phase
-    /// TODO: * complementing a complex number
-    /// TODO: * summing up or subtracting two complex numbers
-    /// TODO: * representing a complex number as a string or the form Re +/- iIm
-    /// TODO:     - e.g. via the ToString() method
-    /// TODO: * checking whether two complex numbers are equal or not
-    /// TODO:     - e.g. via the Equals(object) method
+    /// Model Complex numbers in an object-oriented way and implement this class.
+    /// In other words, you must provide a means for:
+    /// * instantiating complex numbers
+    /// * accessing a complex number's real, and imaginary parts
+    /// * accessing a complex number's modulus, and phase
+    /// * complementing a complex number
+    /// * summing up or subtracting two complex numbers
+    /// * representing a complex number as a string or the form Re +/- iIm
+    ///     - e.g. via the ToString() method
+    /// * checking whether two complex numbers are equal or not
+    ///     - e.g. via the Equals(object) method
     public class Complex
     {
-        // TODO: fill this class\
+        public double Real { get; set; }
+        public double Imaginary { get; set; }
+
+        public Complex(double real, double imaginary)
+        {
+            this.Real = real;
+            this.Imaginary = imaginary;
+        }
+
+        public double Modulus => Math.Sqrt(Math.Pow(this.Real, 2) + Math.Pow(this.Imaginary, 2));
+
+        public double Phase => Math.Atan(this.Imaginary / this.Real);
+
+        public Complex Complement() => new Complex(this.Real, -this.Imaginary);
+
+        public Complex Plus(Complex b) => new Complex(this.Real + b.Real, this.Imaginary + b.Imaginary);
+
+        public Complex Minus(Complex b) => new Complex(this.Real - b.Real, this.Imaginary - b.Imaginary);
+
+        public String ToString()
+        {
+            String str = $"{this.Real}";
+            str = (this.Imaginary >= 0 ? $"{str}+{this.Imaginary}i" : $"{str}{this.Imaginary}i");
+            return str;
+        }
+
+        public bool Equals(Complex b) => this.Real.Equals(b.Real) && this.Imaginary.Equals(b.Imaginary);
+
     }
 }
