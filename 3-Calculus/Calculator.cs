3d223d20490f31a1a282f260c8dde36cabb71fe7
Anny <anny.bevilacqua@studio.unibo.it>
2021-12-08 19:04:11 +0100

    Resolve Calculus

--- 3-Calculus/Calculator.cs
+++ 3-Calculus/Calculator.cs
@@ -1,3 +1,4 @@
+using System;
 using ComplexAlgebra;
 
 namespace Calculus
@@ -20,13 +21,66 @@ namespace Calculus
     /// HINT: - a property/method to let the user request an operation
     /// HINT: - the usual ToString() method, which is helpful for debugging
     /// HINT: - you may exploit as many _private_ fields/methods/properties as you like
-    ///
-    /// TODO: implement the calculator class in such a way that the Program below works as expected
     class Calculator
     {
         public const char OperationPlus = '+';
         public const char OperationMinus = '-';
 
-        // TODO fill this class
+        private Complex _value;
+        private char _operation;
+
+        public Complex Value
+        {
+            get => _value; 
+            set
+            {
+                _value = value;
+            }
+        }
+
+        private Complex Result { get; set; }
+
+        public char Operation
+        {
+            get => _operation;
+            set
+            {
+                ComputeResult();
+                _operation = value;
+                Value = null;
+            }
+        }
+
+        public void Reset()
+        {
+            Value = null;
+            _operation = '\0';
+        }
+
+        public void ComputeResult()
+        {
+            if (Operation.Equals(OperationPlus) && Result != null)
+            {
+                Result = Result.Plus(Value);
+            }
+            else if (Operation.Equals(OperationMinus) && Result != null)
+            {
+                Result = Result.Minus(Value);
+            }
+            else
+            {
+                Result = Value;
+            }
+            _operation = '\0';
+            Value = Result;
+        }
+        
+        public override String ToString()
+        {
+            String val = Value == null ? "null" : Value.ToString();
+            String op = Operation == '\0' ? "null" : _operation.ToString();
+            return $"{val}, {op}";
+        }
+        
     }
 }
