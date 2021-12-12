214979a79806eb66f6cc5b40cf4e109032389bf6
Anny <anny.bevilacqua@studio.unibo.it>
2021-12-12 16:30:31 +0100

    Add EqualsTest, fix fields typo

--- 6-IndexersTests/Map2DTests.cs
+++ 6-IndexersTests/Map2DTests.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace Indexers
 {
     using System.Linq;
@@ -6,15 +8,15 @@ namespace Indexers
     [TestClass]
     public class Map2DTests
     {
-        private readonly int[] seven = new int[] { 7, 14, 21, 28, 35, 42, 49, 56, 63, 70 };
-        private IMap2D<int, int, int> pitagoricTable;
+        private readonly int[] _seven = new int[] { 7, 14, 21, 28, 35, 42, 49, 56, 63, 70 };
+        private IMap2D<int, int, int> _pythagoreanTable;
 
         // Called once before each test after the constructor
         [TestInitialize]
         public void TestInitialize()
         {
-            this.pitagoricTable = new Map2D<int, int, int>();
-            this.pitagoricTable.Fill(
+            this._pythagoreanTable = new Map2D<int, int, int>();
+            this._pythagoreanTable.Fill(
                 Enumerable.Range(1, 10),
                 Enumerable.Range(1, 10),
                 (i, j) => i * j);
@@ -23,25 +25,42 @@ namespace Indexers
         [TestMethod]
         public void FillAndIndexerTest()
         {
+            Console.WriteLine(_pythagoreanTable.ToString());
+
             for (int i = 1; i <= 10; i++)
             {
-                Assert.AreEqual(i * i, this.pitagoricTable[i, i]);
+                Assert.AreEqual(i * i, this._pythagoreanTable[i, i]);
             }
         }
 
         [TestMethod]
         public void GetRowTest()
         {
-            if (!this.pitagoricTable.GetRow(7).Select(t => t.Item2).SequenceEqual(this.seven))
+            if (!this._pythagoreanTable.GetRow(7).Select(t => t.Item2).SequenceEqual(this._seven))
             {
                 Assert.Fail("Wrong implementation");
             }
         }
 
         [TestMethod]
+        public void EqualsTest()
+        {
+            var newMap2D = new Map2D<int, int, int>();
+            newMap2D.Fill(
+                Enumerable.Range(1, 10),
+                Enumerable.Range(1, 10),
+                (i, j) => i * j);
+
+            if (!this._pythagoreanTable.Equals(newMap2D))
+            {
+                Assert.Fail("Wrong implementation");
+            }
+        }
+        
+        [TestMethod]
         public void GetColumnTest()
         {
-            if (!this.pitagoricTable.GetColumn(7).Select(t => t.Item2).SequenceEqual(this.seven))
+            if (!this._pythagoreanTable.GetColumn(7).Select(t => t.Item2).SequenceEqual(this._seven))
             {
                 Assert.Fail("Wrong implementation");
             }
